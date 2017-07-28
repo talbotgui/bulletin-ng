de499006b8be9ddcd5c34df8b74a88d3889f5b2c
Guillaume TALBOT <talbotgui@gmail.com>
2017-07-28 23:11:01 +0200

    Affichage des onglets uniquement quand une année est chargée

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,15 @@
 import {Component} from '@angular/core';
 
+import {DataService} from './service/data.service';
+
 @Component({selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css']})
 export class AppComponent {
+
+  constructor(private dataService: DataService) {}
+
+  // Condition d'affichage des onglets
+  get anneeChargee() {
+    return this.dataService.isAnneeChargee();
+  }
 
 }
