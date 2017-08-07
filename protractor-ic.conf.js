424683faa6c741e39a628a2476144bc60958081d
Guillaume TALBOT <talbotgui@gmail.com>
2017-08-07 14:23:19 +0200

    Ajout d'une configuration Protractor pour l'IC

--- protractor-ic.conf.js
+++ protractor-ic.conf.js
@@ -13,5 +13,6 @@ exports.config = {
 	onPrepare() {
 		require('ts-node').register({ project: 'e2e/tsconfig.e2e.json' });
 		jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-	}
+	},
+	chromeOptions: { args: [ "--headless", "--disable-gpu", "--window-size=800x600" ] }
 };
