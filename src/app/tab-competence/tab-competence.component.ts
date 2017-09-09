676e61e72496e436c2b3b944bd66378dbacedd65
Guillaume TALBOT <talbotgui@gmail.com>
2017-09-09 21:13:00 +0200

    Modification du filtrage dans l'onglet "compétences" pour utiliser le filtrage plus complet codé dans le service

--- src/app/tab-competence/tab-competence.component.ts
+++ src/app/tab-competence/tab-competence.component.ts
@@ -1,27 +1,44 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-
+import { MdSnackBar } from '@angular/material';
 import { TreeComponent } from 'angular-tree-component';
 
 import * as model from '../model/model';
 import { LectureService } from '../service/lecture.service';
 
-export class Noeud { constructor(public id: string, public idParent: string, public name: string, public children: Noeud[]) { } }
+export class Noeud {
+  data: any;
+  constructor(public id: string, public idParent: string, public name: string, public children: Noeud[]) { }
+}
 
 @Component({ selector: 'tab-competence', templateUrl: './tab-competence.component.html', styleUrls: ['./tab-competence.component.css'] })
 export class TabCompetenceComponent implements OnInit {
 
-  // Filtre de recherche dans l'arbre
-  filtre: string;
+  // Compétence sélectionnnée
+  libelleCompletCompetenceSelectionnee: string;
 
   // Noeuds à afficher
   noeuds: Noeud[] = [];
+
+  // Filtre de recherche dans l'arbre
+  private tmpfiltre: string;
+  private motsDuFiltre: string[];
+
+  set filtre(valeur: string) {
+    this.tmpfiltre = valeur;
+    if (valeur) {
+      this.motsDuFiltre = valeur.toUpperCase().split(' ');
+    }
+  }
+  get filtre() {
+    return this.tmpfiltre;
+  }
 
   // Instance de l'abre
   @ViewChild(TreeComponent)
   private tree: TreeComponent;
 
   // Un constructeur pour se faire injecter les dépendances
-  constructor(private lectureService: LectureService) { }
+  constructor(private lectureService: LectureService, private snackBar: MdSnackBar) { }
 
   // Appel au service à l'initialisation du composant
   ngOnInit(): void {
@@ -55,12 +72,25 @@ export class TabCompetenceComponent implements OnInit {
         } else {
           console.error('Le parent n\'est pas déclaré avant l\'enfant dans le fichier de données');
         }
-
       }
     }
   }
 
   recherche() {
-    this.tree.treeModel.filterNodes(this.filtre, true);
+    if (this.motsDuFiltre && this.motsDuFiltre[0].length > 3) {
+      this.tree.treeModel.filterNodes(
+        (node: Noeud) => {
+          let resultat = true;
+          if (this.motsDuFiltre && this.motsDuFiltre[0].length > 3 && node.data.name) {
+            resultat = this.lectureService.compareLibelleCompetencePourRecherche(node.data.name.toUpperCase(), this.motsDuFiltre);
+          }
+          return resultat;
+        }
+      );
+    }
+  }
+
+  onActivationDunNoeud(idCompetence: string): void {
+    this.libelleCompletCompetenceSelectionnee = this.lectureService.getLibelleCompletCompetence(idCompetence);
   }
 }
