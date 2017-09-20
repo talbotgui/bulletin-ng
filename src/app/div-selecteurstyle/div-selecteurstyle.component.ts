35cff7b47f208c08198aa25aae734bb5173e8f3b
Guillaume TALBOT <talbotgui@gmail.com>
2017-09-20 07:04:01 +0200

    Pas de code dans un constructeur (utilisation de onInit)

--- src/app/div-selecteurstyle/div-selecteurstyle.component.ts
+++ src/app/div-selecteurstyle/div-selecteurstyle.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MdDialog } from '@angular/material';
 
 import { DataRepository } from '../service/data.repository';
 
 @Component({ selector: 'div-selecteurstyle', templateUrl: './div-selecteurstyle.component.html' })
-export class DivSelecteurStyleComponent {
+export class DivSelecteurStyleComponent implements OnInit {
 
-  constructor(private dataRepository: DataRepository) {
+  constructor(private dataRepository: DataRepository) { }
+
+  // Appel au service à l'initialisation du composant
+  ngOnInit(): void {
     // Pour initialiser le theme au démarrage de l'application
     this.dataRepository.setThemeSelectionne(this.dataRepository.getThemeSelectionne());
   }
