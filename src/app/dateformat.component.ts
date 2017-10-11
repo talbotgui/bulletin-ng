88b663be9d43c45c6196dee137278b0a9e247f99
Guillaume TALBOT <talbotgui@gmail.com>
2017-10-11 19:30:09 +0200

    Correction des pbs de dates cliquées/saisies dans les datePicker

--- src/app/dateformat.component.ts
+++ src/app/dateformat.component.ts
@@ -10,7 +10,9 @@ export class MyDateAdapter extends NativeDateAdapter {
     parse(value: any): Date | null {
         if ((typeof value === 'string') && (value.indexOf('/') > -1)) {
             const str = value.split('/');
-            return new Date(Number(str[2]), Number(str[1]) - 1, Number(str[0]), 12);
+            const date = new Date(Number(str[2]), Number(str[1]) - 1, Number(str[0]), 12);
+            date.setHours(0, 0, 0, 0);
+            return date;
         }
         const timestamp = typeof value === 'number' ? value : Date.parse(value);
         return isNaN(timestamp) ? null : new Date(timestamp);
